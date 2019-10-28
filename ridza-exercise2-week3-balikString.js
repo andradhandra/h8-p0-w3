72c79d119a50e3d467a2c5c80bd545fec624058d
Ridza Adhandra <andradhandra@gmail.com>
2019-10-28 14:33:31 +0700

    Exercise2 Week3 Ridza

---
+++ ridza-exercise2-week3-balikString.js
@@ -0,0 +1,14 @@
+function balikString(kata) {
+    var atak = "";
+    for (i=kata.length-1; i>=0; i--) {
+        atak +=  kata[i];
+    } 
+    return atak;
+}
+
+//test case
+console.log(balikString("kasur"));
+console.log(balikString("rusak"));
+console.log(balikString("kasur rusak"));
+console.log(balikString("tebet"));
+console.log(balikString("sop"));
