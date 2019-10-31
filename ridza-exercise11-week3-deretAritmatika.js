21b2fefba465ac15559da926739ccce982a1551f
Ridza Adhandra <andradhandra@gmail.com>
2019-10-31 11:40:42 +0700

    exercise11 week3 ridza

---
+++ ridza-exercise11-week3-deretAritmatika.js
@@ -0,0 +1,17 @@
+function tentukanDeretAritmatika(arr) {
+    // you can only write your code here!
+    var a = []
+    for (var i=arr.length-1; i>0; i--) {
+        var bukti = arr[i] - arr[i-1];
+        if (bukti != (arr[arr.length-1]-arr[arr.length-2])) {
+        return false; 
+        } 
+    } return true;
+  }
+  
+  // TEST CASES
+  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 5, 6])); // true
+  console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
+  console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
+  console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
+  console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
